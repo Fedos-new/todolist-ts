83434a8031a8a306006cc10e90103a1f0f620e22
Fedos-new <fedluky@gmail.com>
2021-03-01 23:32:22 +0700

    Updates

---
+++ precache-manifest.dbcf324da4ea890b1501280bf2143a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b6d9168c26e4184d51d16a8d9bc765",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "8b0550afd29f8eebe71c",
+    "url": "/todolist-ts/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "0314b914e9dbdbd78530",
+    "url": "/todolist-ts/static/js/2.edf87164.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist-ts/static/js/2.edf87164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0550afd29f8eebe71c",
+    "url": "/todolist-ts/static/js/main.78cdd164.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
